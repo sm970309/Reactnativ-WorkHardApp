1cf3deea7d749eb3e52e7bfa68af585121ffcf28
sm970309 <sm970309@gmail.com>
2022-05-02 16:33:33 +0900

    Add TextInput

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TextInput } from 'react-native';
+import { useState } from 'react';
 import { theme } from './color';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const [text,setText] = useState('');
+  const onChangeText = (event) =>{
+    setText(event)
+  }
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text style={{ ...styles.btnText, color: working ? 'white' : theme.grey }}>Work</Text>
         </TouchableOpacity>
-        <TouchableHighlight onPress={() => console.log('pressed')}>
-          <Text style={styles.btnText}>Travel</Text>
+        <TouchableHighlight onPress={travel}>
+          <Text style={{ ...styles.btnText, color: !working ? 'white' : theme.grey }}>Travel</Text>
         </TouchableHighlight>
+      </View>
+      <View>
+        <TextInput
+          onChangeText={onChangeText}
+          placeholder={working ? "Add a To Do" : "Where do you wanna go?"}
+          style={styles.input} />
+        <Text style={{ ...styles.btnText, color: 'white' }}>{text}</Text>
       </View>
     </View>
   );
@@ -22,16 +37,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.bg,
-    paddingHorizontal:20,
+    paddingHorizontal: 20,
   },
-  header:{
-    justifyContent:'space-between',
-    flexDirection:"row",
-    marginTop:100,
+  header: {
+    justifyContent: 'space-between',
+    flexDirection: "row",
+    marginTop: 100,
   },
-  btnText:{
-    fontSize:44,
-    fontWeight:"bold",
-    color:'white'
+  btnText: {
+    fontSize: 44,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18
   }
 });
