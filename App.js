c699f46d6ff8510d11891647c28b580e4ddc72d8
sm970309 <sm970309@gmail.com>
2022-05-02 15:02:54 +0900

    Add Touchable

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { theme } from './color';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableHighlight onPress={() => console.log('pressed')}>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableHighlight>
+      </View>
     </View>
   );
 }
@@ -13,8 +21,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal:20,
   },
+  header:{
+    justifyContent:'space-between',
+    flexDirection:"row",
+    marginTop:100,
+  },
+  btnText:{
+    fontSize:44,
+    fontWeight:"bold",
+    color:'white'
+  }
 });
